b15b6d92935e01adc028306a1b3aaa187c33dce2
Emad <mohammed.gad@student.giu-uni.de>
2024-11-26 19:29:09 +0200

    Successfully hashed password. User related services should be working perfectly

--- personal-finance-manager-backend/app/schemas/userSchema.py
+++ personal-finance-manager-backend/app/schemas/userSchema.py
@@ -12,6 +12,11 @@ class UserCreate(UserBase):
     password: str
 
 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
 class UserUpdate(BaseModel):
     email: Optional[EmailStr] = None
     name: Optional[str] = None
--- personal-finance-manager-backend/app/services/auth_service.py
+++ personal-finance-manager-backend/app/services/auth_service.py
@@ -1,21 +1,11 @@
-# services/auth_service.py
-from app.models.user import User
-from app.core.database import SessionLocal
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
+from app.models import userModel
+from app.core.security import verify_password, create_access_token
+from app.schemas.userSchema import UserLogin
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
-
-def create_user(db: Session, name: str, email: str, password: str):
-    hashed_password = pwd_context.hash(password)
-    db_user = User(name=name, email=email, password=hashed_password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+def authenticate_user(email: str, password: str, db: Session):
+    user = db.query(userModel.User).filter(userModel.User.email.is_(email)).first()
+    if not user or not verify_password(password, user.hashed_password):
+        return None
+    return create_access_token(subject=user.id)
